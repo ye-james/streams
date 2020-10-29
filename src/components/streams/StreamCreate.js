504f1a64d33858a06adeca1f58a0ef0acb748e4e
James <james101994@yahoo.com>
2020-10-28 17:55:50 -0700

    Added validation to stream creation inputs

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,25 +3,62 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
 
-    renderInput({ input, label }) {
+    renderError({error, touched}) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    renderInput = ({ input, label, meta }) => {
+
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
         return (
-            <div>
+            <div className={className}>
                 <label>{label}</label>
-                <input {...input} />
+                <input {...input} autoComplete="off"/>
+                {this.renderError(meta)}
             </div>
         );
     }
 
+    onFormSubmit(formValues) {
+        console.log(formValues);
+    }   
+
     render() {
+        console.log(this.props);
         return (
-            <form className="ui form">
+            <form className="ui form error" onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
                 <Field name="title" label="Enter Title" component={this.renderInput}/>
-                <Field name="description" label="Enter Description" component={this.renderInput}/>
+                <Field name="description" label="Enter Description" component={this.renderInput} />
+                <button className="ui button primary">Submit</button>
             </form>
         )
     }
 }
 
+const validate = formValues => {
+    const errors = {};
+
+    if (!formValues.title) {
+        //only ran if user did not enter a title
+        errors.title = 'There must be a title'
+    }
+
+        if (!formValues.description) {
+        //only ran if user did not enter a dedscription
+        errors.description = 'There must be a description'
+    }
+    return errors;
+}
+
 export default reduxForm({
-    form:'streamCreate'
+    form: 'streamCreate',
+    validate: validate
 })(StreamCreate);
