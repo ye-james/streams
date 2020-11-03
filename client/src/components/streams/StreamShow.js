3e02db0b4216177ec53524f539204153d3fe9aef
James <james101994@yahoo.com>
2020-11-03 15:44:47 -0800

    Implement video player using Node Media Server to stream OBS live streams and flv.js

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,8 +1,62 @@
 import React from 'react'
+import flv from 'flv.js'
+import { connect } from 'react-redux';
+import { fetchStream } from '../../redux/actions'
 
-const StreamShow = () => {
+class StreamShow extends React.Component {
 
-    return <div>StreamShow </div>
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if (this.playerl || !this.props.stream) return;
+        const { id } = this.props.stream;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+
+    }
+
+
+    render() {
+
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        const { title, description } = this.props.stream;
+        return (
+            <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls/>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        );
+    }
+
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    };
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
